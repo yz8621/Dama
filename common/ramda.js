27a1de82c1e00afeb5e74a0822a9532ba74597a6
Yasmin Zaraket <92760985+yz8621@users.noreply.github.com>
2025-08-11 18:58:45 +0100

    Update ramda.js

--- common/ramda.js
+++ common/ramda.js
@@ -2,5 +2,6 @@
  * This is an integration fix, so that Ramda can be imported in the same way
  * both on server and on browser, and also not trip jslint.
  */
-import * as R from "./node_modules/ramda/es/ramda_index.js";
+//import * as R from "./node_modules/ramda/es/ramda_index.js";
+import * as R from "https://esm.sh/ramda@0.29.1";
 export default R;
